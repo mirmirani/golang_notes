e09f7dda986949188f75e6fe2f8f5bda0f063933
Mir Mirani <mir.mirani@outlook.com>
2020-08-10 15:20:53 +0100

    my notes

--- notes.go
+++ notes.go
@@ -380,6 +380,20 @@ func big_numbers2() {
 
 }
 
+func big_numbers2() {
+	lightSpeed := big.NewInt(299792)
+	secondsPerDay := big.NewInt(86400)
+	distance := new(big.Int)
+	distance.SetString("24000000000000000000", 10)
+	fmt.Println("Andromeda Galaxy is", distance, "km away.")
+	seconds := new(big.Int)
+	seconds.Div(distance, lightSpeed)
+	days := new(big.Int)
+	days.Div(seconds, secondsPerDay)
+	fmt.Println("That is", days, "days of travel at light speed.")
+
+}
+
 func main() {
 
 	rand.Seed(time.Now().UnixNano())
